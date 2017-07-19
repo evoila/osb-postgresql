f9e1550acfdc18bb00a64abdba8b45521e7ff31e
René Schollmeyer <rschollmeyer@evoila.de>
2017-07-19 11:35:46 +0200

    refactor(property beans): move property beans to corresponding projects

--- core/src/main/java/de/evoila/cf/broker/bean/MongoDBSecurityKeyBean.java
+++
@@ -1,6 +0,0 @@
-package de.evoila.cf.broker.bean;
-
-public interface MongoDBSecurityKeyBean {
-
-	public int getLength();
-}
--- core/src/main/java/de/evoila/cf/broker/bean/RabbitMQSecurityKeyBean.java
+++
@@ -1,7 +0,0 @@
-package de.evoila.cf.broker.bean;
-
-public interface RabbitMQSecurityKeyBean {
-
-	public int getLength();
-	
-}
