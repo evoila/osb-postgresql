21ada69c9f3266eaaaf3101a395ce8ca5fb62f78
René Schollmeyer <rschollmeyer@evoila.de>
2017-07-19 11:36:46 +0200

    refactor(property beans): move property beans to corresponding projects

---
+++ persistence-mongo/src/main/java/de/evoila/cf/broker/bean/DataMongoDBBean.java
@@ -0,0 +1,11 @@
+package de.evoila.cf.broker.bean;
+
+public interface DataMongoDBBean {
+
+	public String getHost();
+	public int getPort();
+	public String getDatabase();
+	public String getUsername();
+	public String getPassword();
+	
+}
