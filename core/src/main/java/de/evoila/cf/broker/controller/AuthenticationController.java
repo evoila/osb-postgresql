1d78b0070901765be0fe2c3c4ec824d6f5530633
Johannes Hiemer <jhiemer@evoila.de>
2017-08-03 16:32:55 +0200

    feat(auth-mechanism): external auth mechanism for Uaa Tokens

--- core/src/main/java/de/evoila/cf/broker/controller/AuthenticationController.java
+++ core/src/main/java/de/evoila/cf/broker/controller/AuthenticationController.java
@@ -3,43 +3,6 @@
  */
 package de.evoila.cf.broker.controller;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URISyntaxException;
-import java.util.Collections;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import evoila.cf.broker.openid.OpenIdAuthenticationUtils;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.utils.URIBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.codec.Base64;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
-import org.springframework.stereotype.Controller;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
-
 import de.evoila.cf.broker.controller.utils.DashboardAuthenticationRedirectBuilder;
 import de.evoila.cf.broker.controller.utils.DashboardUtils;
 import de.evoila.cf.broker.model.Dashboard;
@@ -49,14 +12,37 @@ import de.evoila.cf.broker.model.ServiceInstance;
 import de.evoila.cf.broker.model.oauth.CompositeAccessToken;
 import de.evoila.cf.broker.repository.ServiceInstanceRepository;
 import de.evoila.cf.broker.service.CatalogService;
+import evoila.cf.broker.openid.OpenIdAuthenticationUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 /**
  * @author Johannes Hiemer.
  *
  */
 @Controller
-@RequestMapping(value = "/v2/dashboard")
-public class DashboardController extends BaseController {
+@RequestMapping(value = "/v2/authentication")
+public class AuthenticationController extends BaseController {
 	
 	private final Logger log = LoggerFactory.getLogger(getClass());
 	
@@ -111,10 +97,11 @@ public class DashboardController extends BaseController {
     }
 
 	@RequestMapping(value = "/{serviceInstanceId}/confirm")
-    public Object confirm(@PathVariable String serviceInstanceId, @RequestParam(value = "code", required = true) String authCode,
-						  HttpServletRequest request) throws URISyntaxException {
+    public ModelAndView confirm(@PathVariable String serviceInstanceId, @RequestParam(value = "code", required = true) String authCode,
+								HttpServletRequest request) throws URISyntaxException {
+		ModelAndView mav = new ModelAndView("index");
 		if (authCode == null)
-			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+			mav.setViewName("unauthorized");
 
 		ServiceDefinition serviceDefinition = resolveServiceDefinitionByServiceInstanceId(serviceInstanceId);
 		if (serviceDefinition != null) {
@@ -130,36 +117,16 @@ public class DashboardController extends BaseController {
 				log.info("Token could not be used for Dashboard please reauthenticate...");
 			}
 
+
 			if (token != null) {
-				this.createTokenAuthenticationContext(token.getAccessToken(), request);
-				log.info("Creating User Session for Dashboard after successful authentication...");
+				mav.addObject("token", token);
+				mav.addObject("serviceInstanceId", serviceInstanceId);
 			} else {
+				mav.setViewName("unauthorized");
 				log.info("Did not receive a valid token, had to abort authentication...");
 			}
 		}
-		return "redirect:/v2/dashboard/manage/" + serviceInstanceId;
+		return mav;
 	}
-
-	@RequestMapping(value = "/manage/{serviceInstanceId}")
-    public Object manage(@PathVariable String serviceInstanceId,
-    		HttpServletRequest request) throws URISyntaxException {
-
-		return "manage";
-    }
-
-	private void createTokenAuthenticationContext(String token, HttpServletRequest request) {
-		UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken(token, token);
-		Authentication authentication = authenticationManager.authenticate(authRequest);
-		SecurityContext securityContext = SecurityContextHolder.getContext();
-		securityContext.setAuthentication(authentication);
-
-		this.setAuthenticatedSession(securityContext, true);
-	}
-    
-    private void setAuthenticatedSession(SecurityContext securityContext, boolean isAuthenticated) {
-    	httpSession.setAttribute(IS_AUTHENTICATED, isAuthenticated);
-    	httpSession.setMaxInactiveInterval(SESSION_TIMEOUT);
-		httpSession.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
-    }
 
 }
