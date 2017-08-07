5cca71f8665e2d785a9082071acb882cf9d68e36
Johannes Hiemer <jhiemer@evoila.de>
2017-08-07 16:43:23 +0200

    typo(removed-declarations): Removed unnecessary HashMap declarations

--- core/src/main/java/de/evoila/cf/broker/controller/BackupController.java
+++ core/src/main/java/de/evoila/cf/broker/controller/BackupController.java
@@ -16,52 +16,50 @@ import java.util.HashMap;
 class BackupController extends BaseController {
 
     @Autowired
-    BackupService backupService;
+    private BackupService backupService;
 
     @RequestMapping(value = "/{serviceInstanceId}/backup", method = RequestMethod.POST)
     public ResponseEntity<HashMap> backupNow(@PathVariable String serviceInstanceId, @RequestBody HashMap fileDestination) throws ServiceInstanceDoesNotExistException {
         ResponseEntity<HashMap> response = backupService.backupNow(serviceInstanceId, fileDestination);
-        return new ResponseEntity<HashMap>(response.getBody(), response.getStatusCode());
+        return new ResponseEntity<>(response.getBody(), response.getStatusCode());
     }
 
     @RequestMapping(value = "/{serviceInstanceId}/restore", method = RequestMethod.POST)
     public ResponseEntity<HashMap> restoreNow(@PathVariable String serviceInstanceId, @RequestBody HashMap fileDestination) throws ServiceInstanceDoesNotExistException {
         ResponseEntity<HashMap> response = backupService.restoreNow(serviceInstanceId, fileDestination);
-        return new ResponseEntity<HashMap>(response.getBody(), response.getStatusCode());
+        return new ResponseEntity<>(response.getBody(), response.getStatusCode());
     }
-
-    // JOBS
 
     @RequestMapping(value = "/{serviceInstanceId}/jobs", method = RequestMethod.GET)
     public ResponseEntity<HashMap> getJobs(@PathVariable String serviceInstanceId,
-                                            @PageableDefault(size = 50, page = 0) Pageable pageable) {
+                                            @PageableDefault(size = 50) Pageable pageable) {
         ResponseEntity<HashMap> response = backupService.getJobs(serviceInstanceId, pageable);
-        return new ResponseEntity<HashMap>(response.getBody(), response.getStatusCode());
+        return new ResponseEntity<>(response.getBody(), response.getStatusCode());
     }
 
     @RequestMapping(value = "/{serviceInstanceId}/jobs/{jobid}", method = RequestMethod.GET)
     public ResponseEntity<HashMap> getJobs(@PathVariable String serviceInstanceId, @PathVariable String jobid) {
         ResponseEntity<HashMap> response = backupService.getJob(serviceInstanceId, jobid);
-        return new ResponseEntity<HashMap>(response.getBody(), response.getStatusCode());
+        return new ResponseEntity<>(response.getBody(), response.getStatusCode());
     }
 
     @RequestMapping(value = "/{serviceInstanceId}/jobs/{jobid}", method = RequestMethod.DELETE)
     public ResponseEntity<HashMap> deleteJobs(@PathVariable String serviceInstanceId, @PathVariable String jobid) {
         ResponseEntity<HashMap> response = backupService.deleteJob(serviceInstanceId, jobid);
-        return new ResponseEntity<HashMap>(response.getBody(), response.getStatusCode());
+        return new ResponseEntity<>(response.getBody(), response.getStatusCode());
     }
     // PLAN
     @RequestMapping(value = "/{serviceInstanceId}/plans", method = RequestMethod.GET)
     public ResponseEntity<HashMap> getPlans(@PathVariable String serviceInstanceId,
-                                            @PageableDefault(size = 50, page = 0) Pageable pageable) {
+                                            @PageableDefault(size = 50) Pageable pageable) {
         ResponseEntity<HashMap> response = backupService.getPlans(serviceInstanceId, pageable);
-        return new ResponseEntity<HashMap>(response.getBody(), response.getStatusCode());
+        return new ResponseEntity<>(response.getBody(), response.getStatusCode());
     }
 
     @RequestMapping(value = "/{serviceInstanceId}/plans", method = RequestMethod.POST)
     public ResponseEntity<HashMap> postPlan(@PathVariable String serviceInstanceId, @RequestBody HashMap plan) throws ServiceInstanceDoesNotExistException {
         ResponseEntity<HashMap> response = backupService.postPlan(serviceInstanceId, plan);
-        return new ResponseEntity<HashMap>(response.getBody(), response.getStatusCode());
+        return new ResponseEntity<>(response.getBody(), response.getStatusCode());
     }
 
 
@@ -69,7 +67,7 @@ class BackupController extends BaseController {
     public ResponseEntity<HashMap> getPlan(@PathVariable String serviceInstanceId,
                                              @PathVariable String planId) throws ServiceInstanceDoesNotExistException {
         ResponseEntity<HashMap> response = backupService.getPlan(serviceInstanceId, planId);
-        return new ResponseEntity<HashMap>(response.getBody(), response.getStatusCode());
+        return new ResponseEntity<>(response.getBody(), response.getStatusCode());
     }
 
     @RequestMapping(value = "/{serviceInstanceId}/plans/{planId}", method = RequestMethod.PATCH)
@@ -77,13 +75,13 @@ class BackupController extends BaseController {
                                              @PathVariable String planId,
                                              @RequestBody HashMap plan) throws ServiceInstanceDoesNotExistException {
         ResponseEntity<HashMap> response = backupService.updatePlan(serviceInstanceId, planId, plan);
-        return new ResponseEntity<HashMap>(response.getBody(), response.getStatusCode());
+        return new ResponseEntity<>(response.getBody(), response.getStatusCode());
     }
 
     @RequestMapping(value = "/{serviceInstanceId}/plans/{planId}", method = RequestMethod.DELETE)
     public ResponseEntity<HashMap> deletePlan(@PathVariable String serviceInstanceId,
                                               @PathVariable String planId) {
         ResponseEntity<HashMap> response = backupService.deletePlan(serviceInstanceId, planId);
-        return new ResponseEntity<HashMap>(response.getBody(), response.getStatusCode());
+        return new ResponseEntity<>(response.getBody(), response.getStatusCode());
     }
 }
