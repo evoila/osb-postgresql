b8e105a229e0ea53961a9945ecb44a9704d98b24
Yannic Remmet <yremmet@evoila.de>
2017-08-14 15:33:23 +0200

    fix(existingservice) removes wrong import

--- existing-service/src/main/java/de/evoila/cf/cpi/existing/ExistingServiceFactory.java
+++ existing-service/src/main/java/de/evoila/cf/cpi/existing/ExistingServiceFactory.java
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import de.evoila.cf.broker.bean.impl.AuthenticationPropertiesBeanImpl;
 import de.evoila.cf.broker.bean.impl.ExistingEndpointBeanImpl;
 import de.evoila.cf.broker.exception.PlatformException;
 import de.evoila.cf.broker.model.Plan;
