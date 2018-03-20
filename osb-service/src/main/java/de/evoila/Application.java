0c1eadc976bf35a0df318df113f07dfff4d1873c
Johannes Hiemer <jhiemer@evoila.de>
2018-03-20 22:08:03 +0100

    Cleaining up the shit

--- osb-service/src/main/java/de/evoila/Application.java
+++ osb-service/src/main/java/de/evoila/Application.java
@@ -4,8 +4,11 @@
 package de.evoila;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.system.ApplicationPidFileWriter;
+import org.springframework.cloud.bus.BusAutoConfiguration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -22,7 +25,7 @@ import java.util.Map;
  */
 @RefreshScope
 @SpringBootApplication
-@EnableMongoRepositories(basePackages={"de.evoila.cf.cpi.openstack.custom", "de.evoila.cf.broker.persistence.mongodb.repository"})
+@EnableAutoConfiguration(exclude = { RabbitAutoConfiguration.class, BusAutoConfiguration.class })
 public class Application {
 
 	@Bean(name = "customProperties")
--- osb-service/src/main/resources/docker/container.cmd
+++
@@ -1,8 +0,0 @@
-export REPOSITORY_POSTGRES=$repo_service &&
-export REPOSITORY_MAIN=$repos_main &&
-apt-get update &&
-apt-get install -y wget &&
-wget $repo_service/postgres-template.sh && 
-chmod +x postgres-template.sh  &&
-./postgres-template.sh -d $database_name -u $database_user -p $database_password -e docker &&
-top
